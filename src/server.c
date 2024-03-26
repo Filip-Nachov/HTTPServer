79142f66f09e1db03f8fc7ca9d9e9b25e9b8cc2a
Seven719 <ilie.andronie@gmail.com>
2024-03-26 16:42:29 +0000

    Created main function
    Created error function
    
    Added potentially needed libraries

---
+++ src/server.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+void error(char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+
+}
