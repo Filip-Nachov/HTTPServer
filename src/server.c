2ce64f7977ae4476c72635ec377ca0cc02685e30
Seven719 <ilie.andronie@gmail.com>
2024-03-29 08:22:41 +0000

    Accepts Connections

--- src/server.c
+++ src/server.c
@@ -1,4 +1,5 @@
 #include <asm-generic/socket.h>
+#include <unistd.h>
 #include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -10,35 +11,51 @@
 #define PORT 6969
 
 int main(int argc, char *argv[]) {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int sockFd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (sockfd == -1) {
+    if (sockFd == -1) {
         printf("Could not create socket: %s...\n", strerror(errno));
         return 1;
     }
 
     int reuse = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(&reuse)) < 0){
+    if (setsockopt(sockFd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(&reuse)) < 0){
         printf("Could not reuse socket: %s...\n", strerror(errno));
         return 1;
     }
 
-    struct sockaddr_in serv_addr = {
+    struct sockaddr_in servAddr = {
         .sin_family = AF_INET,
         .sin_addr.s_addr = INADDR_ANY,
         .sin_port = htons(PORT),
     };
 
-    if (bind(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+    if (bind(sockFd, (struct sockaddr*) &servAddr, sizeof(servAddr)) < 0) {
         printf("Could not bind socket: %s...\n", strerror(errno));
         return 1;
     }
 
     int backlog = 5;
-    if (listen(sockfd, backlog) < 0) {
+    if (listen(sockFd, backlog) < 0) {
         printf("Listen failed: %s...\n", strerror(errno)); 
         return 1;
     }
+
+    printf("Waiting for a client to connect...\n");
+    
+    struct sockaddr_in clientAddr;
+    socklen_t clientAddrLen = sizeof(clientAddr);
+    int clientFd = accept(sockFd, (struct sockaddr*) &clientAddr, &clientAddrLen);
+    
+    if (clientFd < 0) {
+        printf("Could not accept connection: %s...\n", strerror(errno));
+        return 1;
+    }
+
+    printf("Client Connected\n");
+
+    close(clientFd);
+    close(sockFd);
 
     return EXIT_SUCCESS;
 }
