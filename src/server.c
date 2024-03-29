ab822c94c35e3ad6eb01a88f337226f882d86dc2
Seven719 <ilie.andronie@gmail.com>
2024-03-29 07:39:15 +0000

    Created Socket
    Made socket reusable

--- src/server.c
+++ src/server.c
@@ -1,14 +1,23 @@
+#include <asm-generic/socket.h>
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <string.h>
 #include <sys/socket.h>
 
-void error(char *msg) {
-    perror(msg);
-    exit(1);
-}
-
 int main(int argc, char *argv[]) {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
+    if (sockfd == -1) {
+        printf("Could not create socket: %s...\n", strerror(errno));
+        return 1;
+    }
+
+    int reuse = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(&reuse)) < 0){
+        printf("Could not reuse socket: %s...\n", strerror(errno));
+    }
+
+    return EXIT_SUCCESS;
 }
