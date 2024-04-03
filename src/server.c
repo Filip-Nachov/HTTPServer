4d88a1dae44b3579d5991a1588368f9b48892fc1
Seven719 <ilie.andronie@gmail.com>
2024-04-03 10:50:02 +0100

    Added functionality for handleClient function
    Added future functions at the top

--- src/server.c
+++ src/server.c
@@ -15,6 +15,46 @@
 #define MAX_SIZE 4096
 
 void *handleClient(void *p_clientFd);
+const char *getFileExtension(const char *file_name);
+void buildResponse(const char *fileName, const char *fileExt, char *response, ssize_t *responseLen);
+
+void *handleClient(void *p_clientFd) {
+    int clientFd = *((int*)p_clientFd);
+    free(p_clientFd);
+    char buf[1024] = {0}, bufCopy[1024] = {0};
+    ssize_t bytesReceived = recv(clientFd, buf, sizeof(buf), 0);
+
+    if (bytesReceived < 0) {
+        printf("Could not get buffer: %s...\n", strerror(errno));
+        return NULL;
+    }
+
+    strcpy(bufCopy, buf);
+    char *method = strtok(bufCopy, " ");
+    char *filePath = strtok(NULL, " ");
+    strtok(NULL, "\r\n");
+
+    if (strcmp(method, "GET") != 0) {
+        printf("Only accept GET...");
+        return NULL;
+    }
+
+    char fileExt[32] = {0};
+    char fileName[256];
+    strcpy(fileExt, getFileExtension(filePath));
+    strncpy(fileName, filePath, strlen(filePath) - strlen(fileExt) - 1);
+    fileName[strlen(filePath) - strlen(fileExt) - 1] = '\0';
+
+    char *response = (char *)malloc(MAX_SIZE * 2);
+    ssize_t responseLen;
+    buildResponse(fileName, fileExt, response, &responseLen);
+
+    send(clientFd, response, responseLen, 0);
+
+    printf("%s\n", fileExt);
+    close(clientFd);
+    return NULL;
+}
 
 int main(int argc, char *argv[]) {
     int sockFd = socket(AF_INET, SOCK_STREAM, 0);
